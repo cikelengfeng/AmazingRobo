cff7afa01886e3485bb02f98d2a376dfa5961ee4
cikelengfeng <502805412@qq.com>
2013-12-18 17:58:46 +0800

    修改文件前缀

---
+++ AmazingRobo/AmazingRobo/TestRunner/ARTestRunner.h
@@ -0,0 +1,13 @@
+//
+//  ARTestRunner.h
+//  AmazingRobo
+//
+//  Created by 徐 东 on 13-12-18.
+//  Copyright (c) 2013年 DeanXu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ARTestRunner : NSObject
+
+@end
---
+++ AmazingRobo/AmazingRobo/TestRunner/ARTestRunner.m
@@ -0,0 +1,13 @@
+//
+//  ARTestRunner.m
+//  AmazingRobo
+//
+//  Created by 徐 东 on 13-12-18.
+//  Copyright (c) 2013年 DeanXu. All rights reserved.
+//
+
+#import "ARTestRunner.h"
+
+@implementation ARTestRunner
+
+@end
